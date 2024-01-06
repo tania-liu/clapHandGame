dc96f3ff664bfd62188dabc5d486e4934d62967d
Tania Liu <tanialiu430006@gmail.com>
2024-01-06 14:37:46 +0800

    Initial Commit

---
+++ clapHandGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clapHandGame
+//
+//  Created by 劉俐廷 on 2024/1/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
